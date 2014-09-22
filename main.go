309d146bbc5a2d088eebbc235eeb3d8fc3f95cf9
joyrexus <joyrexus@gmail.com>
2014-09-22 09:41:35 -0500

    add new handler

--- main.go
+++ main.go
@@ -2,8 +2,49 @@ package main
 
 import (
     "fmt"
+    "encoding/json"
+    "net/http"
+    "strings"
 )
 
 func main() {
-    fmt.Println("Hello world!")
+    http.HandleFunc("/hello", hello)
+
+    http.HandleFunc("/weather/", weather)
+
+    http.ListenAndServe(":8080", nil)
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte("hi!"))
+}
+
+func weather(w http.ResponseWriter, r *http.Request) {
+    city := strings.SplitN(r.URL.Path, "/", 3)[2]
+    fmt.Println(city)
+    w.Write([]byte(city))
+}
+
+func query(city string) (weatherData, error) {
+    resp, err := http.Get("http://api.openweathermap.org/data/2.5/weather?q=" + city)
+    if err != nil {
+        return weatherData{}, err
+    }
+
+    defer resp.Body.Close()
+
+    var d weatherData
+
+    if err := json.NewDecoder(resp.Body).Decode(&d); err != nil {
+        return weatherData{}, err
+    }
+
+    return d, nil
+}
+
+type weatherData struct {
+    Name string `json:"name"`
+    Main struct {
+        Kelvin float64 `json:"temp"`
+    } `json:"main"`
 }
